ae57e3f35058a6963051a3a19483d6f65ba40a8d
teofrastusb <44885309+teofrastusb@users.noreply.github.com>
2018-11-08 22:05:22 -0600

    Update PlantGrowth.py

--- PlantGrowth.py
+++ PlantGrowth.py
@@ -11,12 +11,13 @@ import os
 import time
 
 # Manually set constants
-NumSpacesX = 30
-NumSpacesY = 10
+numSpacesX = 30
+numSpacesY = 10
 
-PlantID = 1
-NumPlants = 20
-NumSlimes = 2
+plantID = 1
+slimeID = 2
+numPlants = 20
+numSlimes = 2
 
 SCREEN_WIDTH = 1200
 SCREEN_HEIGHT = 600
@@ -25,6 +26,7 @@ x = 0
 y = 0
 turn = 0
 
+mapMatrix = []
 # Setup classes for needed objects
 class Plant:
     """
@@ -47,38 +49,62 @@ class Slime:
     xp = 0
 
 # Global list
-plantlist = [Plant() for i in range(NumPlants)]
+plantlist = [Plant() for i in range(numPlants)]
+slimelist = [Slime() for i in range(numSlimes)]
 
 def makeMatrix():
     # Setup an empty matrix of the correct size
-    MapArray = []
 
-    for i in range(NumSpacesX):
-        MapArray.append([0]*NumSpacesY)
+    for i in range(numSpacesX):
+        mapMatrix.append([0]*numSpacesY)
         
-    return MapArray
 
-def plantPlants(MapArray):
+
+def plantPlants():
     # Set a given number of points in the matix to be plants spots
     PlantCount = 0
-    while PlantCount < NumPlants:
-        randX = random.randint(0,NumSpacesX/2-1)
-        randY = random.randint(0,NumSpacesY-1)
+    while PlantCount < numPlants:
+        randX = random.randint(0,numSpacesX/2-1)
+        randY = random.randint(0,numSpacesY-1)
     
         #print(randX,randY)
     
-        if MapArray[randX][randY] == 0:
+        if mapMatrix[randX][randY] == 0:
         
             plantlist[PlantCount].x = randX
             plantlist[PlantCount].y = randY
         
-            plantlist[PlantCount+1].x = NumSpacesX-randX
-            plantlist[PlantCount+1].y = NumSpacesY-randY
+            plantlist[PlantCount+1].x = numSpacesX-randX
+            plantlist[PlantCount+1].y = numSpacesY-randY
         
             PlantCount = PlantCount+2
 
-            MapArray[randX][randY] = PlantID
-            MapArray[NumSpacesX-1-randX][NumSpacesY-1-randY] = PlantID 
+            mapMatrix[randX][randY] = plantID
+            mapMatrix[numSpacesX-1-randX][numSpacesY-1-randY] = plantID
+
+                
+            
+def placeSlimes():
+    # Set a given number of points in the matix to be plants spots
+    slimecount = 0
+    while slimecount < numSlimes:
+        randX = random.randint(0,numSpacesX/2-1)
+        randY = random.randint(0,numSpacesY-1)
+    
+        #print(randX,randY)
+    
+        if mapMatrix[randX][randY] == 0:
+        
+            slimelist[slimecount].x = randX
+            slimelist[slimecount].y = randY
+        
+            slimelist[slimecount+1].x = numSpacesX-randX
+            slimelist[slimecount+1].y = numSpacesY-randY
+        
+            slimeCount = slimecount+2
+
+            mapMatrix[randX][randY] = slimeID
+            mapMatrix[numSpacesX-1-randX][numSpacesY-1-randY] = slimeID
 
 class MyGame(arcade.Window):
     """ Main application class. """
@@ -97,14 +123,20 @@ class MyGame(arcade.Window):
         
         arcade.set_background_color(arcade.color.ALMOND)
 
-        StepY = (SCREEN_HEIGHT//(NumSpacesY+2))
-        StepX = (SCREEN_WIDTH//(NumSpacesX+2))
+        StepY = (SCREEN_HEIGHT//(numSpacesY+2))
+        StepX = (SCREEN_WIDTH//(numSpacesX+2))
 
-        for i in range(NumPlants):
+        for i in range(numPlants):
             x = (plantlist[i].x+1)*StepX
             y = (plantlist[i].y+1)*StepY
-            radius = (SCREEN_HEIGHT//NumSpacesY)//4 * plantlist[i].level/10
+            radius = (SCREEN_HEIGHT//numSpacesY)//4 * plantlist[i].level/10
             arcade.draw_circle_filled(x, y, radius, arcade.color.GREEN)
+            
+        for i in range(numSlimes):
+            x = (slimelist[i].x+1)*StepX
+            y = (slimelist[i].y+1)*StepY
+            radius = (SCREEN_HEIGHT//numSpacesY)//2
+            arcade.draw_circle_filled(x, y, radius, arcade.color.BLUE)
 
         # Put the text on the screen.
         output = "turn: {}".format(turn)
@@ -117,7 +149,7 @@ class MyGame(arcade.Window):
         global turn
         turn += 1
         
-        for i in range(NumPlants):
+        for i in range(numPlants):
             UpgradeChance = random.randint(0,20)//1
             if UpgradeChance > plantlist[i].level:
                 plantlist[i].level += 1
@@ -128,8 +160,9 @@ class MyGame(arcade.Window):
 
 
 def main():
-    MapArray = makeMatrix()
-    plantPlants(MapArray)
+    makeMatrix()
+    plantPlants()
+    placeSlimes()
     MyGame()
     arcade.run()
 
