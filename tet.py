565867449d43c0e6691ff6a197281c84b65b6bf3
teofrastusb <44885309+teofrastusb@users.noreply.github.com>
2018-11-10 11:37:26 -0600

    Create tet.py

---
+++ tet.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# This was my attempt to get an external code to control the slimes
+class Tet():
+
+    def tet():
+        # Setup an empty matrix of the correct size
+        print("tet function")
+        #return "Left"
+"""
+if __name__ == '__main__':
+    # test1.py executed as script
+    # do something
+    tet()
+"""
