d4277fe5d67e34500aab9fc46f2cc0292eed7238
Ian Kottman <ikottman@users.noreply.github.com>
2018-11-11 22:12:50 -0600

    Manage slime list in main

--- models/player_base.py
+++ models/player_base.py
@@ -2,9 +2,6 @@ import random
 from models.commands import Commands
 
 class PlayerBase():
-    def __init__(self):
-        self.commands = Commands
-
     def command_slime(self, map, slime):
         """
           Player's should override this method to provide an AI for each slime they control.
@@ -12,4 +9,4 @@ class PlayerBase():
           Invalid or impossible commands (e.g. moving onto a taken coordinate or eating nothing) will be ignored.
         """
         # default implementation is a random selection from all commands
-        return random.choice(list(self.commands))
+        return random.choice(list(Commands))
