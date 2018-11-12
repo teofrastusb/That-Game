604f03e27ea12257ef76c6273fb4ae94b2fb2d0b
unknown <teofrastusb@gmail.com>
2018-11-11 19:26:54 -0600

    Added Movment function to the MyGame class, and made some minor changes to how plants are planted

--- main.py
+++ main.py
@@ -71,6 +71,18 @@ class MyGame(arcade.Window):
 
         arcade.set_background_color(arcade.color.ALMOND)
 
+    # Evaluate and exicute command
+    def movement(self,command,slime,column_count, row_count):
+        if command is Commands.UP and slime.y < self.map.column_count():
+            slime.y +=1
+        elif command is Commands.DOWN and slime.y > 0:
+            slime.y -=1
+        elif command is Commands.RIGHT and slime.x < self.map.row_count():
+            slime.x +=1
+        elif command is Commands.LEFT and slime.x >0:
+            slime.x -=1
+        return slime
+
     def setup(self):
         """ Initialize game state """
         # place slimes
@@ -80,7 +92,7 @@ class MyGame(arcade.Window):
                 self.conf['slimes'].getfloat('sprite_scaling'),
                 self.map)
         # Create the plants
-        for i in range(10):
+        for i in range(self.conf['plants'].getint('num_total')//2):
             rand_x = random.randint(1, self.map.row_count() / 2)
             rand_y = random.randint(1, self.map.column_count())
             # left half
@@ -89,9 +101,9 @@ class MyGame(arcade.Window):
             self.all_sprites_list.append(plant)
             self.plant_list.append(plant)
 
-            # mirrored right half
+            # mirrored across x and y axis for right half
             plant = Plant(i, self.conf, self.map)
-            plant.set_coord(self.map.row_count() - rand_x, rand_y)
+            plant.set_coord(self.map.row_count() - rand_x,self.map.column_count() - rand_y)
             self.all_sprites_list.append(plant)
             self.plant_list.append(plant)
 
@@ -130,7 +142,7 @@ class MyGame(arcade.Window):
         # TODO: include slimes
         for plant in self.plant_list:
             # TODO: I think this should be all_sprites_list once we add in slimes to it
-            # level up plant for everything it hits that isn't a plant
+            # level up plant for everything it hits that isn't a plant or on a chance basis?
             hits = arcade.check_for_collision_with_list(plant, slimeList1)
             hits = hits + arcade.check_for_collision_with_list(plant, slimeList2)
             for hit in hits:
@@ -146,15 +158,8 @@ class MyGame(arcade.Window):
         for i in range(self.conf['slimes'].getint('num_one')):
             command = self.player_one.command_slime(self.map, {})
             
-            # Evaluate and exicute command
-            if command is Commands.UP and slimeList1[i].y < self.map.column_count():
-                slimeList1[i].y +=1
-            elif command is Commands.DOWN and slimeList1[i].y > 0:
-                slimeList1[i].y -=1
-            elif command is Commands.RIGHT and slimeList1[i].x < self.map.row_count():
-                slimeList1[i].x +=1
-            elif command is Commands.LEFT and slimeList1[i].x >0:
-                slimeList1[i].x -=1
+            slimeList1[i].x=MyGame.movement(self,command,slimeList1[i],self.map.column_count(),self.map.row_count()).x
+            slimeList1[i].y=MyGame.movement(self,command,slimeList1[i],self.map.column_count(),self.map.row_count()).y
             
             # Check for collisions
             collision = 0
@@ -181,15 +186,8 @@ class MyGame(arcade.Window):
             
             command = self.player_two.command_slime(self.map, {})
 
-            # Evaluate and exicute command
-            if command is Commands.UP and slimeList2[i].y < self.map.column_count():
-                slimeList2[i].y +=1
-            if command is Commands.DOWN and slimeList2[i].y > 0:
-                slimeList2[i].y -=1
-            if command is Commands.RIGHT and slimeList2[i].x < self.map.row_count():
-                slimeList2[i].x +=1
-            if command is Commands.LEFT and slimeList2[i].x >0:
-                slimeList2[i].x -=1
+            slimeList2[i].x=MyGame.movement(self,command,slimeList2[i],self.map.column_count(),self.map.row_count()).x
+            slimeList2[i].y=MyGame.movement(self,command,slimeList2[i],self.map.column_count(),self.map.row_count()).y
 
             # Check for collisions
             collision = 0
