e22953e98e60284186bceb98de454e7440f26c24
unknown <teofrastusb@gmail.com>
2018-11-12 18:24:17 -0600

    Adding sprite_man to manage sprites

--- models/sprite_man.py
+++ models/sprite_man.py
@@ -10,3 +10,16 @@ class Sprite_man():
     def update(self):
         # nothin to do...yet
         pass
+
+    def check_for_dead(self):
+        # Bring out your dead!
+        self.kill_list = []
+
+        for plant in self.plant_list:
+            if plant.hp == 0
+                self.kill_list.append(plant)
+                
+        self.plant_list = arcade.SpriteList()
+        self.slimes_one = arcade.SpriteList()
+        self.slimes_two = arcade.SpriteList()
+        self.all_sprites_list = arcade.SpriteList()
