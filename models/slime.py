bea39b69fb186a356dabe814fff90a2cfcc51f44
teofrastusb <teofrastusb@gmail.com>
2018-11-14 22:27:59 -0600

    test

--- models/slime.py
+++ models/slime.py
@@ -14,13 +14,13 @@ class Slime(arcade.Sprite):
         self.max_level = config['slimes'].getint('max_level')
         self.current_hp = config['slimes'].getint('max_hp')
         self.max_hp = config['slimes'].getint('max_hp')
-        self.hp_increment = self.max_hp
+        self.hp_increment = config['slimes'].getint('hp_increment')
         self.attack = config['slimes'].getint('attack')
-        self.attack_increment = self.attack
+        self.attack_increment = config['slimes'].getint('attack_increment')
 
     def update(self):
         # Check level, then ...
-        self.level_up()
+        self.level_check()
 
     def set_coord(self, x, y):
         self.x = x
@@ -32,7 +32,7 @@ class Slime(arcade.Sprite):
     def split(self):
         self.xp = self.xp // 2.2
 
-    def level_up(self):
+    def level_check(self):
         # Save current level
         starting_level = self.level
 
@@ -43,16 +43,23 @@ class Slime(arcade.Sprite):
         if self.level >= self.max_level:
             self.level = self.max_level
 
-        # If the slime leveled up do the following
-        if self.level > starting_level:
+        # Update attack based on level up
+        self.attack = self.level*self.attack_increment
 
-            # Change max hp on level up
-            self.max_hp += self.hp_increment
+        # Update max hp based on level
+        self.max_hp = self.level*self.hp_increment
 
-            # Add hp on level up
+        # Add hp on level up
+        if starting_level < self.level:
             self.current_hp += self.hp_increment
-            if self.current_hp >= self.max_hp:
-                self.current_hp = self.max_hp
+            print('xp', self.xp)
+            print('level', self.level)
+            print('max hp', self.max_hp)
+            print('current hp', self.current_hp)
+        
+        # make sure current hp isn't above max hp
+        if self.current_hp >= self.max_hp:
+            self.current_hp = self.max_hp
 
-            # Add attack on level up
-            self.attack += self.attack_increment
+
+            
